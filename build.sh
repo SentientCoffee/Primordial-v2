40ec6fe78bffc027a2bb042173302c7bf3c2b079
Daniel <22991229+SentientCoffee@users.noreply.github.com>
2023-01-23 20:50:08 -0500

    [Update] Build.sh script

--- build.sh
+++ build.sh
@@ -8,8 +8,24 @@ BUILD_LEVEL="speed"
 DEBUG_FLAG=""
 VET_FLAG=""
 
-while getopts ":debug:vet:" opt ; do
-    case "${opt}" in
+bold_blue="\x1b[94;1m"
+bold_red="\x1b[91;1m"
+reset="\x1b[0m"
+
+if [ "${1}" = "debug" ] ; then
+    EXE_NAME="${EXE_NAME}_d"
+    BUILD_LABEL="debug"
+    BUILD_LEVEL="minimal"
+    DEBUG_FLAG="-debug"
+fi
+
+[ "${1}" = "vet" ] && VET_FLAG="-vet"
+[ "${2}" = "vet" ] && VET_FLAG="-vet"
+
+ARGS=$(printf "%s\n" $@)
+
+for arg in ${ARGS}; do
+    case "${arg}" in
         debug)
             EXE_NAME="${EXE_NAME}_d"
             BUILD_LABEL="debug"
@@ -21,7 +37,10 @@ while getopts ":debug:vet:" opt ; do
             VET_FLAG="-vet"
         ;;
 
-        *) ;;
+        *)
+            printf "${bold_red}Unknown option ${opt}!${reset}"
+            exit 1
+        ;;
     esac
 done
 
@@ -30,17 +49,15 @@ BUILD_DIR="build/${BUILD_LABEL}"
 [ -d "${BUILD_DIR}" ] && rm -rf "${BUILD_DIR}"
 mkdir -p "${BUILD_DIR}"
 
-printf -n "Building ${BUILD_LABEL} binaries"
+printf "${bold_blue}Building ${BUILD_LABEL} binaries"
 [ -n "${VET_FLAG}" ] && printf " (vetted)"
-printf "...\n==========\n"
+printf "...\n==========\n${reset}"
 
-echo "Shader compiler:"
+printf "${bold_blue}Shader compiler:\n${reset}"
 odin run shadercomp -out:${BUILD_DIR}/${SHADERCOMP_NAME} -o:${BUILD_LEVEL} ${DEBUG_FLAG} ${VET_FLAG} -microarch:native -show-timings
-[ "$?" -gt 0 ] && echo -e "==========\nBuild failed (shader compiler)!" && exit 1
+[ "$?" -gt 0 ] && printf "${bold_red}==========\nBuild failed (shader compiler)!${reset}" && exit 1
 
-echo "=========="
-echo "Main program:"
-
+printf "${bold_blue}==========\nMain program:\n${reset}"
 odin build primordial -out:${BUILD_DIR}/${MAIN_EXE_NAME} -o:${BUILD_LEVEL} ${DEBUG_FLAG} ${VET_FLAG} -microarch:native -show-timings
-[ "$?" -gt 0 ] && printf "==========\nBuild failed (Primordial)!" && exit 1
-printf "==========\nBuild successful."
+[ "$?" -gt 0 ] && printf "${bold_red}==========\nBuild failed (Primordial)!${reset}" && exit 1
+printf "${bold_blue}==========\nBuild successful.${reset}"
