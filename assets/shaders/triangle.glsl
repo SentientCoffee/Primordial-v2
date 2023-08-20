43a8d68bec44996c757dfdecce6845c928daba82
Daniel <22991229+SentientCoffee@users.noreply.github.com>
2023-08-20 13:44:59 -0400

    [Add] Texture mapping!

--- assets/shaders/triangle.glsl
+++ assets/shaders/triangle.glsl
@@ -8,20 +8,28 @@ layout(binding = 0) uniform Matrices {
 
 layout(location = 0) in vec3 i_position;
 layout(location = 1) in vec3 i_color;
+layout(location = 2) in vec2 i_uv;
 
-layout(location = 0) out vec3 o_frag_color;
+layout(location = 0) out vec3 o_color;
+layout(location = 1) out vec2 o_uv;
 
 void main() {
     gl_Position = mtx.proj * mtx.view * mtx.model * vec4(i_position, 1.0);
-    o_frag_color = i_color;
+    o_color = i_color;
+    o_uv = i_uv;
 }
 
 #type FRAGMENT
 
-layout(location = 0) in vec3 i_frag_color;
+layout(binding = 1) uniform sampler2D u_sampler;
+
+layout(location = 0) in vec3 i_color;
+layout(location = 1) in vec2 i_uv;
 
 layout(location = 0) out vec4 o_color;
 
 void main() {
-    o_color = vec4(i_frag_color, 1.0);
+    vec4 color = texture(u_sampler, i_uv);
+    color *= vec4(i_color, 1.0);
+    o_color = color;
 }
