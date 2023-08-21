a6dd2d8f052c50d357163e787d61a2c485532759
Daniel <22991229+SentientCoffee@users.noreply.github.com>
2023-08-21 02:23:49 -0400

    [Add] Depth buffering, use cube

--- assets/shaders/triangle.glsl
+++ assets/shaders/triangle.glsl
@@ -4,7 +4,7 @@ layout(binding = 0) uniform Matrices {
     mat4 model;
     mat4 view;
     mat4 proj;
-} mtx;
+} u_mtx;
 
 layout(location = 0) in vec3 i_position;
 layout(location = 1) in vec3 i_color;
@@ -14,7 +14,7 @@ layout(location = 0) out vec3 o_color;
 layout(location = 1) out vec2 o_uv;
 
 void main() {
-    gl_Position = mtx.proj * mtx.view * mtx.model * vec4(i_position, 1.0);
+    gl_Position = u_mtx.proj * u_mtx.view * u_mtx.model * vec4(i_position, 1.0);
     o_color = i_color;
     o_uv = i_uv;
 }
@@ -31,5 +31,6 @@ layout(location = 0) out vec4 o_color;
 void main() {
     vec4 color = texture(u_sampler, i_uv);
     color *= vec4(i_color, 1.0);
+    // vec4 color = vec4(i_color, 1.0);
     o_color = color;
 }
